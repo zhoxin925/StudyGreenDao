62141ff3cb1d2afde6c0029bc8ee1f10c02232a7
zhoxin925 <2013268994@qq.com>
2019-07-21 22:42:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/studygreendao/bean/Student.java
@@ -0,0 +1,91 @@
+package com.example.studygreendao.bean;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Unique;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class Student {
+    @Id(autoincrement = true)
+    private Long id;
+    @Unique
+    private int studentNo;
+    private int age;
+    private String telPhone;
+    private String sex;
+    private String name;
+    private String address;
+    private String schoolName;
+    private String grade;
+    @Generated(hash = 1071002287)
+    public Student(Long id, int studentNo, int age, String telPhone, String sex,
+            String name, String address, String schoolName, String grade) {
+        this.id = id;
+        this.studentNo = studentNo;
+        this.age = age;
+        this.telPhone = telPhone;
+        this.sex = sex;
+        this.name = name;
+        this.address = address;
+        this.schoolName = schoolName;
+        this.grade = grade;
+    }
+    @Generated(hash = 1556870573)
+    public Student() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public int getStudentNo() {
+        return this.studentNo;
+    }
+    public void setStudentNo(int studentNo) {
+        this.studentNo = studentNo;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getTelPhone() {
+        return this.telPhone;
+    }
+    public void setTelPhone(String telPhone) {
+        this.telPhone = telPhone;
+    }
+    public String getSex() {
+        return this.sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getSchoolName() {
+        return this.schoolName;
+    }
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+    public String getGrade() {
+        return this.grade;
+    }
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
